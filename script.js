effc0c739a150a87461dee3310648bc0f3d21729
Nikola Krstevski <36802068+nikolaesp@users.noreply.github.com>
2022-11-10 15:08:03 +0100

    fatto prezzo

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 let distanza = prompt(`Inserisci kilometri da percorere: `);
 let eta = prompt(`Inserisci la tua eta: `);
 let costo = distanza * 0.21;
+let prezzo;
+if(eta<18){
+    prezzo = costo*0.2;
+}else{
+
+if(eta>65){
+    prezzo=costo * 0.4;
+}else{
+    prezzo=costo;
+}
+}
