1ce3c8f953556a6e7a900bca1ccbe55fd57c68a3
Nikola Krstevski <36802068+nikolaesp@users.noreply.github.com>
2022-11-10 16:55:22 +0100

    fatto con la graffica anche

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let distanza = prompt(`Inserisci kilometri da percorere: `);
+/*
+let distanza = prompt(`Inserisci kilometri: `);
 let eta = prompt(`Inserisci la tua eta: `);
 let costo = distanza * 0.21;
 let prezzo;
@@ -12,7 +13,34 @@ if(eta>65){
     prezzo=costo;
 }
 }
+*/
+let btn = document.getElementById("btncalc");
 
+btn.addEventListener('click',function(){
+
+let distanza = document.getElementById("idistanza").value;
+let eta = document.getElementById("ieta").value;
+eta = parseInt(eta);
+let costo = parseFloat( distanza * 0.21);
+let prezzo;
+if(eta<18){
+    prezzo = costo*0.8;
+}else{
+
+if(eta>65){
+    prezzo=costo * 0.6;
+}else{
+    prezzo=costo;
+}
+}
+let tarifa = parseFloat(prezzo).toFixed(2);
+
+document.getElementById("oprezzo").innerHTML = `Prezzo per viaggo:  `+ tarifa+`\u20AC`;
+
+});
+
+
+/*
 let display = `
 --------------------------------
 Eta :              ${eta} anni
@@ -21,3 +49,4 @@ Prezzo per viaggo: ${parseFloat(prezzo).toFixed(2)} \u20AC
 --------------------------------`;
 
 console.log(display);
+*/
